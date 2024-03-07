0683a721cfeebeb8920ae2aea9682be7c3917f9e
isshosan <rasyidridhom@gmail.com>
2024-03-07 18:08:09 +0700

    first commit

---
+++ Learn with ChatGPT.py
@@ -0,0 +1,14 @@
+# Easy
+for i in range(5):
+    print("Menggambar lingkaran ke-", i+1)
+
+# Medium
+counter = 0
+while counter < 5:
+    print("Menggambar lingkaran ke-", counter+1)
+    counter += 1
+
+# Hard
+colors = ["merah", "kuning", "hijau", "biru", "ungu"]
+for color in colors:
+    print("Menggambar lingkaran berwarna", color)
